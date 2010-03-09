557110f10359a34751a94a9b18abfcef39c9dcb8
Koushik K. Dutta <koushd@gmail.com>
2010-03-09 09:39:23 -0800

    publish latest current version to a static url

--- tools/makerecoveries.sh
+++ tools/makerecoveries.sh
@@ -12,7 +12,9 @@ mkdir -p $PUBLISH_PATH
 for lunchoption in aosp_dream_us-eng aosp_sapphire_us-eng cyanogen_passion-eng cyanogen_sholes-eng koush_magic_us-eng koush_hero-eng
 do
     lunch $lunchoption
+    rm -rf $OUT/obj/EXECUTABLES/recovery_intermediates
     DEVICE_NAME=$(echo $TARGET_PRODUCT | sed s/koush_// | sed s/aosp_// | sed s/_us// | sed s/cyanogen_//)
     make -j16 recoveryimage 
     ccp $OUT/recovery.img recoveries/recovery-clockwork-$1-$DEVICE_NAME.img
+    ccp $OUT/recovery.img recoveries/recovery-clockwork-$DEVICE_NAME.img
 done
