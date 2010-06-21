6c8ab5e6065b7be589e2e5b22c8ee1c4c0966f79
Koushik Dutta <koushd@gmail.com>
2010-06-21 08:54:41 -0700

    add more products

--- tools/makerecoveries.sh
+++ tools/makerecoveries.sh
@@ -6,7 +6,7 @@ fi
 
 if [ -z "$2" ]
 then
-    PRODUCTS='koush_supersonic-eng koush_bravo-eng koush_dream-eng koush_sapphire-eng koush_passion-eng cyanogen_sholes-eng koush_magic-eng koush_hero-eng koush_heroc-eng koush_desirec-eng'
+    PRODUCTS='koush_inc-eng koush_supersonic-eng koush_bravo-eng koush_dream-eng koush_sapphire-eng koush_passion-eng cyanogen_sholes-eng koush_magic-eng koush_hero-eng koush_heroc-eng koush_desirec-eng'
 else
     PRODUCTS=$2
 fi
