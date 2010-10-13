6f23fb3fe9e532c2a2bccf3ba3a9d67bb4bae3c5
Koushik Dutta <koushd@gmail.com>
2010-10-13 13:04:12 -0700

    buzz, salsa, new inc kernel

--- products/koush_salsa.mk
+++ products/koush_salsa.mk
@@ -1,17 +1,17 @@
 $(call inherit-product, build/target/product/generic.mk)
 
 # How this product is called in the build system
-PRODUCT_NAME := koush_salsa
+PRODUCT_NAME := koush_buzz
 
 # Which actual hardware this is based on (this is a path under vendor/)
-PRODUCT_MANUFACTURER := acer
-PRODUCT_DEVICE := salsa
+PRODUCT_MANUFACTURER := htc
+PRODUCT_DEVICE := buzz
 
 # The user-visible product name
-PRODUCT_MODEL := Acer Liquid
+PRODUCT_MODEL := HTC Buzz (EU)
 
 ifeq ($(TARGET_PRODUCT),$(PRODUCT_NAME))
 TARGET_PREBUILT_RECOVERY_KERNEL := vendor/koush/prebuilt/$(PRODUCT_DEVICE)_recovery_kernel
 endif
 
-include vendor/acer/salsa/device_salsa.mk
+include vendor/htc/buzz/device_buzz.mk
